1cc5370a33a101ab24e835de1c5240dbaaf4c238
scenarioart <yff25960@icloud.com>
2019-10-07 23:52:17 +0900

    get initial count

--- src/scenes/Counter.jsx
+++ src/scenes/Counter.jsx
@@ -1,55 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import socket from '../utils/socket'
+import socket from "../utils/socket";
 
 class Counter extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       count: 0
-    }
+    };
 
-    this.socket = socket
+    this.socket = socket;
   }
 
   componentDidMount() {
-    this.socket.on('RECEIVE_STATE', (data) => { 
-      this.receiveState(data)
+    this.socket.on("RECEIVE_STATE", data => {
+      this.receiveState(data);
     });
+    this.socket.emit("SEND_GET", {});
   }
 
   sendAdd() {
-    this.socket.emit('SEND_ADD', {})
+    this.socket.emit("SEND_ADD", {});
   }
 
   sendReset() {
-    this.socket.emit('SEND_RESET', {})
+    this.socket.emit("SEND_RESET", {});
   }
 
   receiveState(state) {
     this.setState({
       count: state
-    })
+    });
   }
 
   render() {
-    const{ count } = this.state
+    const { count } = this.state;
     return (
       <React.Fragment>
         <p>{count}</p>
-        <button
-          onClick={() => this.sendAdd()}
-        >
-          add
-        </button>
-        <button
-          onClick={() => this.sendReset()}
-        >
-          reset
-        </button>
+        <button onClick={() => this.sendAdd()}>add</button>
+        <button onClick={() => this.sendReset()}>reset</button>
       </React.Fragment>
-    )
+    );
   }
 }
 
